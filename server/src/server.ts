d5c9d43e4c0b6e78dc0ff47318b0bd36767e1515
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-06 13:06:10 -0300

    refactor: change server start message

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,5 +9,5 @@ app.use(express.json())
 app.use(routes)
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log(`server listening on port: ${process.env.PORT || 3333}`)
+  console.log(`server listening on: ${process.env.PORT || 3333}`)
 })
