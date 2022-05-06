f42e17c1e0b20ca4645fb6ec06b8fb2250a6c5f8
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-06 10:36:52 -0300

    refactor: server process env port

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-const port = 3333
+const port = process.env.PORT || 3333
 app.listen(port, () => {
   console.log(`server listening on port: ${port}`)
 })
