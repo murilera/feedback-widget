9295f389635bc40cda2bad87ae5140308426843a
Murilo Rodegheri <murilo.rodegheri@gmail.com>
2022-05-06 13:00:38 -0300

    fix: fix server port

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-const port = process.env.PORT || 3333
-app.listen(port, () => {
-  console.log(`server listening on port: ${port}`)
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`server listening on port: ${process.env.PORT || 3333}`)
 })
